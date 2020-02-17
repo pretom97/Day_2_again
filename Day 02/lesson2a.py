bf59e60952dd7c855309cf9cd009bb597e9089e9
pretom97 <56485431+pretom97@users.noreply.github.com>
2020-02-17 16:02:03 +0600

    Add files via upload

---
+++ Day 02/lesson2a.py
@@ -0,0 +1,31 @@
+print("This is second class") #for_sring_use""
+
+a = ("ali")
+b = (5)
+c = (2.66)
+
+print (a)
+print (b)
+print (c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+p,q,r = (56,34.2,"Hi")
+
+print("p = %s and q= %s and r= %s"%(p,q,r))
+
+#useing_placeholder
+
+print("p = {} and q = {} and r = {}".format(p,q,r))
+
+print("r = {2} and q = {1} and p = {0}".format(p,q,r))
+
+print("We love{}".format(" Nova "))
+
+age = 13
+
+message = "Happy Yeaasin " + str(age) +"th birthday!"
+
+print(message)
